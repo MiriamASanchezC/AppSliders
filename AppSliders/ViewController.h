3f25252fdc04e8426f7653f7e9b09eebac1d8304
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-20 17:56:38 -0600

    App sliders

--- AppSliders/ViewController.h
+++ AppSliders/ViewController.h
@@ -8,6 +8,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UISlider *fontSizeSlider;
+@property (weak, nonatomic) IBOutlet UISlider *alphaSlider;
+
+- (IBAction)fontSize:(id)sender;
+- (IBAction)alpha:(id)sender;
+- (IBAction)enlaceURL:(id)sender;
 
 
 @end
--- AppSliders/ViewController.m
+++ AppSliders/ViewController.m
@@ -19,4 +19,15 @@
 }
 
 
+- (IBAction)enlaceURL:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.google.com"] options:@{} completionHandler:nil];
+}
+
+- (IBAction)alpha:(id)sender {
+    self.label.alpha = self.alphaSlider.value;
+}
+
+- (IBAction)fontSize:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"Futura" size:self.fontSizeSlider.value]];
+}
 @end
