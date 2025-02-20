ceb8fbe2a0d2ce8c4c620d0b23ce4b89dade1bec
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-20 17:37:48 -0600

    Initial Commit

---
+++ AppSliders/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AppSliders
+//
+//  Created by Miriam Sanchez on 20/02/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
